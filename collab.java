94ae2dba2924ea070d39f8f9c07eb6f051d813c3
Praveen Reddy Anugu <75472039+API891@users.noreply.github.com>
2020-12-27 13:21:17 +0530

    Update collab.java

--- collab.java
+++ collab.java
@@ -3,22 +3,5 @@ public class collab {
 	public static void main(String[] args) {
 		System.out.println("Hello Every1");
 		System.out.println("Hello this is praveen");
-System.out.println("Hello this is praveen2");
-System.out.println("Hello this is Raghu01");
-System.out.println("Hello this is Raghu03");
-System.out.println("Hello this is Raghu04");
-System.out.println("Hello this is Raghu05");
-
-}
-=======
-public class collab {
-	public static void main(String[] args) {
-		System.out.println("Hello Every1");
-		System.out.println("Hello this is praveen");
-System.out.println("Hello this is praveen2");
-System.out.println("Hello this is praveen3");
-System.out.println("Hello this is Raghu02");
-
-}
->>>>>>> praveen
 } 
+}
