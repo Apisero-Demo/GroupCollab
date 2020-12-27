f8e3b469e657a49894d7028c31190181479c3601
API889 <75469776+API889@users.noreply.github.com>
2020-12-27 13:47:05 +0530

    Update collab.java

--- collab.java
+++ collab.java
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 public class collab {
 	public static void main(String[] args) {
 		System.out.println("Hello Every1");
 		System.out.println("Hello this is praveen");
-<<<<<<< HEAD
 		System.out.println("Hello this is veena");
-=======
-		System.out.println("Hello this is Veena");
->>>>>>> 2dba0686cf5692378325749e33d89d60a1be0a12
 } 
 }
