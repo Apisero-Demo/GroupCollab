41619ca3bb6c4d565d77f0c1170493d8c52762e4
VeenaVangala <veena.vangala@apisero.com>
2021-01-09 18:41:25 +0530

    collab.java

--- collab.java
+++ collab.java
@@ -1,7 +1,7 @@
 public class collab{
 	public static void main(String[] args){
 		System.out.println("Hello Every1");
-System.out.println("Hello Every1");
+System.out.println("Hello all......");
 
 		
 } 
